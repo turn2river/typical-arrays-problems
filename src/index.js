fb1fb34e85fe351066c48b7b66066e0085ec1f44
alexey.krylov <turn2river@gmail.com>
2021-08-23 23:53:26 +0300

    init: complete typical-arrays-problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
 exports.min = function min (array) {
-  return 0;
+  return !array ? 0 : array.reduce((a, b) => Math.min(a, b), 0);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return !array ? 0 : array.reduce((a, b) => Math.max(a, b), 0) ;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return !array || array.length === 0 ? 0 : array.reduce((a, b) => (a + b)) / array.length;
 }
